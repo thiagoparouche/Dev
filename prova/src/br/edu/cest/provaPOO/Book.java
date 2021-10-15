bcff3b10953d0e22ba23bbc967ec7b93804ad0d1
ThiagoPereira24 <>
2021-10-15 01:25:30 +0000

    Initial commit

---
+++ prova/src/br/edu/cest/provaPOO/Book.java
@@ -0,0 +1,11 @@
+package br.edu.cest.provaPOO;
+
+public class Book extends Item {
+	protected String author;
+	protected String edition;
+	protected String volume;
+	
+	public void Display () {
+		
+	}
+}
---
+++ prova/src/br/edu/cest/provaPOO/Item.java
@@ -0,0 +1,15 @@
+package br.edu.cest.provaPOO;
+
+public class Item {
+	protected String title;
+	protected String publisher;
+	protected String yearpublished;
+	protected String isbn;
+	protected String price;
+	
+	public void Display() {
+		
+	}
+	
+	
+}
